9fd999b3fe45a068af0be9adea7bc5af5aa6bc56
dumari <33925845+dumari@users.noreply.github.com>
2023-08-12 12:26:56 +0200

    Update adres.php

--- adres.php
+++ adres.php
@@ -1,25 +1,7 @@
 <?php
-    $to      = 'nobody@example.com';
-    $subject = 'the subject';
-    $message = 'hello';
-    $headers = 'From: webmaster@example.com'       . "\r\n" .
-                 'Reply-To: webmaster@example.com' . "\r\n" .
-                 'X-Mailer: PHP/' . phpversion();
-
-    mail($to, $subject, $message, $headers);
-$ip = getenv('HTTP_CLIENT_IP')?:
-getenv('HTTP_X_FORWARDED_FOR')?:
-getenv('HTTP_X_FORWARDED')?:
-getenv('HTTP_FORWARDED_FOR')?:
-getenv('HTTP_FORWARDED')?:
-getenv('REMOTE_ADDR');
-
 print "Hello world!";
-print($ipaddress);
-mail("dusica@simula.no","My subject",$ipaddress);
-mail("dusica@simula.no","My subject","Hello world!");
-
 ?>
+
 
 
 
